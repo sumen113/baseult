403bdea0d84e30a32eb5eac0be2e87d987523afa
Sumen113 <sumen113k@gmail.com>
2025-10-21 18:36:49 +1030

    Update view-sidebar.jsx

--- src/icons/view-sidebar.jsx
+++ src/icons/view-sidebar.jsx
@@ -1,15 +1,23 @@
 const ViewSidebar = function () {
     return (
-        <svg
-            class={this.class}
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 -960 960 960"
-            width="24"
-            fill="currentColor"
-        >
-            <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm540-453h100v-107H700v107Zm0 186h100v-106H700v106ZM160-240h460v-480H160v480Zm540 0h100v-107H700v107Z" />
-        </svg>
+<svg
+    class={this.class}
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    width="24"
+    viewBox="0 -960 960 960"
+    fill="none"
+    stroke="white"
+    stroke-width="80"
+    stroke-linejoin="round"
+    stroke-linecap="round"
+>
+    <rect x="160" y="-800" width="280" height="280" rx="60" />
+    <rect x="520" y="-800" width="280" height="280" rx="60" />
+    <rect x="160" y="-440" width="280" height="280" rx="60" />
+    <rect x="520" y="-440" width="280" height="280" rx="60" />
+</svg>
+
     );
 };
 
