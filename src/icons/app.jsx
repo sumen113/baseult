7e467ec6a2170c2ebeb1937e8f80953330943662
Sumen113 <sumen113k@gmail.com>
2025-10-21 18:52:09 +1030

    Change stroke color in SettingsIcon component

--- src/icons/app.jsx
+++ src/icons/app.jsx
@@ -1,6 +1,7 @@
 const SettingsIcon = function () {
   return (
 
+
 <svg
     class={this.class}
     xmlns="http://www.w3.org/2000/svg"
@@ -8,16 +9,21 @@ const SettingsIcon = function () {
     width="24"
     viewBox="0 -960 960 960"
     fill="none"
-    stroke="white"
+    stroke="#ECECEC"
     stroke-width="80"
     stroke-linejoin="round"
     stroke-linecap="round"
 >
+    <!-- Top-left -->
     <rect x="40" y="-920" width="400" height="400" rx="60" />
+    <!-- Top-right -->
     <rect x="520" y="-920" width="400" height="400" rx="60" />
+    <!-- Bottom-left -->
     <rect x="40" y="-440" width="400" height="400" rx="60" />
+    <!-- Bottom-right -->
     <rect x="520" y="-440" width="400" height="400" rx="60" />
 </svg>
+
 
   );
 };
