7395cc100d1d1616304e9a3670a4422b98af1b83
Sumen113 <sumen113k@gmail.com>
2025-10-21 12:40:42 +1030

    Update app.jsx

--- src/icons/app.jsx
+++ src/icons/app.jsx
@@ -1,22 +1,13 @@
 const SettingsIcon = function () {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 32 32"
-      width="24"
-      height="24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="w-6 h-6"
-    >
-      <path d="M14,3H4C3.4,3,3,3.4,3,4v10c0,0.6,0.4,1,1,1h10c0.6,0,1-0.4,1-1V4C15,3.4,14.6,3,14,3z" />
-      <path d="M14,17H4c-0.6,0-1,0.4-1,1v10c0,0.6,0.4,1,1,1h10c0.6,0,1-0.4,1-1V18C15,17.4,14.6,17,14,17z" />
-      <path d="M28,3H18c-0.6,0-1,0.4-1,1v10c0,0.6,0.4,1,1,1h10c0.6,0,1-0.4,1-1V4C29,3.4,28.6,3,28,3z" />
-      <path d="M28,17H18c-0.6,0-1,0.4-1,1v10c0,0.6,0.4,1,1,1h10c0.6,0,1-0.4,1-1V18C29,17.4,28.6,17,28,17z" />
-    </svg>
+<?xml version="1.0" encoding="UTF-8"?>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+<path d="M0 0 C2.64 0 5.28 0 8 0 C8 2.64 8 5.28 8 8 C5.36 8 2.72 8 0 8 C0 5.36 0 2.72 0 0 Z M2 2 C2 3.32 2 4.64 2 6 C3.32 6 4.64 6 6 6 C6 4.68 6 3.36 6 2 C4.68 2 3.36 2 2 2 Z " fill="#ffffff" transform="translate(13,13)"/>
+<path d="M0 0 C2.64 0 5.28 0 8 0 C8 2.64 8 5.28 8 8 C5.36 8 2.72 8 0 8 C0 5.36 0 2.72 0 0 Z M2 2 C2 3.32 2 4.64 2 6 C3.32 6 4.64 6 6 6 C6 4.68 6 3.36 6 2 C4.68 2 3.36 2 2 2 Z " fill="#ffffff" transform="translate(3,13)"/>
+<path d="M0 0 C2.64 0 5.28 0 8 0 C8 2.64 8 5.28 8 8 C5.36 8 2.72 8 0 8 C0 5.36 0 2.72 0 0 Z M2 2 C2 3.32 2 4.64 2 6 C3.32 6 4.64 6 6 6 C6 4.68 6 3.36 6 2 C4.68 2 3.36 2 2 2 Z " fill="#ffffff" transform="translate(13,3)"/>
+<path d="M0 0 C2.64 0 5.28 0 8 0 C8 2.64 8 5.28 8 8 C5.36 8 2.72 8 0 8 C0 5.36 0 2.72 0 0 Z M2 2 C2 3.32 2 4.64 2 6 C3.32 6 4.64 6 6 6 C6 4.68 6 3.36 6 2 C4.68 2 3.36 2 2 2 Z " fill="#ffffff" transform="translate(3,3)"/>
+</svg>
+
   );
 };
 
