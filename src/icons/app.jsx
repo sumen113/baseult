1eb18a1f70687f7e48a9e73bfc136898b74c8cf7
Sumen113 <sumen113k@gmail.com>
2025-10-21 12:42:31 +1030

    Update app.jsx

--- src/icons/app.jsx
+++ src/icons/app.jsx
@@ -1,6 +1,6 @@
 const SettingsIcon = function () {
   return (
-<?xml version="1.0" encoding="UTF-8"?>
+
 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
 <path d="M0 0 C2.64 0 5.28 0 8 0 C8 2.64 8 5.28 8 8 C5.36 8 2.72 8 0 8 C0 5.36 0 2.72 0 0 Z M2 2 C2 3.32 2 4.64 2 6 C3.32 6 4.64 6 6 6 C6 4.68 6 3.36 6 2 C4.68 2 3.36 2 2 2 Z " fill="#ffffff" transform="translate(13,13)"/>
 <path d="M0 0 C2.64 0 5.28 0 8 0 C8 2.64 8 5.28 8 8 C5.36 8 2.72 8 0 8 C0 5.36 0 2.72 0 0 Z M2 2 C2 3.32 2 4.64 2 6 C3.32 6 4.64 6 6 6 C6 4.68 6 3.36 6 2 C4.68 2 3.36 2 2 2 Z " fill="#ffffff" transform="translate(3,13)"/>
