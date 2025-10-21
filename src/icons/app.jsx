d200388f3ace359bfe40d1a7353e33068175664b
Sumen113 <sumen113k@gmail.com>
2025-10-21 18:40:14 +1030

    Refactor SettingsIcon SVG structure and attributes

--- src/icons/app.jsx
+++ src/icons/app.jsx
@@ -1,11 +1,26 @@
 const SettingsIcon = function () {
   return (
 
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32">
-<path d="M0 0 C2.64 0 5.28 0 8 0 C8 2.64 8 5.28 8 8 C5.36 8 2.72 8 0 8 C0 5.36 0 2.72 0 0 Z M2 2 C2 3.32 2 4.64 2 6 C3.32 6 4.64 6 6 6 C6 4.68 6 3.36 6 2 C4.68 2 3.36 2 2 2 Z " fill="#ffffff" transform="translate(13,13)"/>
-<path d="M0 0 C2.64 0 5.28 0 8 0 C8 2.64 8 5.28 8 8 C5.36 8 2.72 8 0 8 C0 5.36 0 2.72 0 0 Z M2 2 C2 3.32 2 4.64 2 6 C3.32 6 4.64 6 6 6 C6 4.68 6 3.36 6 2 C4.68 2 3.36 2 2 2 Z " fill="#ffffff" transform="translate(3,13)"/>
-<path d="M0 0 C2.64 0 5.28 0 8 0 C8 2.64 8 5.28 8 8 C5.36 8 2.72 8 0 8 C0 5.36 0 2.72 0 0 Z M2 2 C2 3.32 2 4.64 2 6 C3.32 6 4.64 6 6 6 C6 4.68 6 3.36 6 2 C4.68 2 3.36 2 2 2 Z " fill="#ffffff" transform="translate(13,3)"/>
-<path d="M0 0 C2.64 0 5.28 0 8 0 C8 2.64 8 5.28 8 8 C5.36 8 2.72 8 0 8 C0 5.36 0 2.72 0 0 Z M2 2 C2 3.32 2 4.64 2 6 C3.32 6 4.64 6 6 6 C6 4.68 6 3.36 6 2 C4.68 2 3.36 2 2 2 Z " fill="#ffffff" transform="translate(3,3)"/>
+<svg
+    class={this.class}
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    width="24"
+    viewBox="0 -960 960 960"
+    fill="none"
+    stroke="white"
+    stroke-width="80"
+    stroke-linejoin="round"
+    stroke-linecap="round"
+>
+    <!-- Top-left -->
+    <rect x="40" y="-920" width="400" height="400" rx="60" />
+    <!-- Top-right -->
+    <rect x="520" y="-920" width="400" height="400" rx="60" />
+    <!-- Bottom-left -->
+    <rect x="40" y="-440" width="400" height="400" rx="60" />
+    <!-- Bottom-right -->
+    <rect x="520" y="-440" width="400" height="400" rx="60" />
 </svg>
 
   );
